ed16e4309d49f46bcc7244ce41434e9b654cee77
jennybc <jenny@stat.ubc.ca>
2014-05-15 15:40:00 -0700

    Explore themes

---
+++ gapminder-ggplot2-themes.r
@@ -0,0 +1,42 @@
+#+ setup, include = FALSE
+library(knitr)
+opts_chunk$set(fig.path = 'figure/themes-')
+
+#' Note: this HTML is made by applying `knitr::spin()` to an R script. So the
+#' narrative is very minimal.
+
+library(ggplot2)
+library(ggthemes)
+
+#' pick a way to load the data
+gdURL <- "http://tiny.cc/gapminder"
+gDat <- read.delim(file = gdURL) 
+gDat <- read.delim("gapminderDataFiveYear.tsv")
+str(gDat)
+
+#' revisit a plot from earlier
+p <- ggplot(gDat, aes(x = gdpPercap, y = lifeExp))
+p <- p + scale_x_log10()
+p <- p + aes(color = continent) + geom_point() + geom_smooth(lwd = 3, se = FALSE)
+p
+
+#' give it a title
+p + ggtitle("Life expectancy over time by continent")
+
+#' change overall look and feel with a premade theme
+p + theme_grey() # the default
+
+#' suppress the usual grey background
+p + theme_bw()
+
+#' exploring some themes from the ggthemes package  
+#' https://github.com/jrnold/ggthemes
+p + theme_calc() + ggtitle("ggthemes::theme_calc()")
+p + theme_economist() + ggtitle("ggthemes::theme_economist()")
+p + theme_economist_white() + ggtitle("ggthemes::theme_economist_white()")
+p + theme_few() + ggtitle("ggthemes::theme_few()")
+p + theme_gdocs() + ggtitle("ggthemes::theme_gdocs()")
+p + theme_tufte() + ggtitle("ggthemes::theme_tufte()")
+p + theme_wsj() + ggtitle("ggthemes::theme_wsj()")
+
+sessionInfo()
